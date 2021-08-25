d89ecbd150d34eb7bc75443dfd888f7ff6359ad2
sebastianblaira <53913977+sebastianblaira@users.noreply.github.com>
2021-08-24 21:46:25 -0500

    Add entities project1

---
+++ todoproject1.Common/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace todoproject1.Common.Models
+{
+    public class Todo
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime EntryTime { get; set; }
+
+        public DateTime OutTime { get; set; }
+
+        public char Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
---
+++ todoproject1.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoproject1.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime EntryTime { get; set; }
+
+        public DateTime OutTime { get; set; }
+
+        public char Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
